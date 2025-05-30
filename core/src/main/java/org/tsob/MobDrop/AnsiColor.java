8edbabd3c76ca2f98cb88450a568f665acf8d9a1
0obriano0 <37872600+0obriano0@users.noreply.github.com>
2025-05-31 03:15:46 +0800

    ansiColor 補全

--- core/src/main/java/org/tsob/MobDrop/AnsiColor.java
+++ core/src/main/java/org/tsob/MobDrop/AnsiColor.java
@@ -11,4 +11,34 @@ public class AnsiColor {
   public static final String PURPLE = "\u001B[35m";
   public static final String CYAN = "\u001B[36m";
   public static final String WHITE = "\u001B[37m";
+
+  public static final String DARK_GRAY = "\u001B[90m";
+
+  public static final String LIGHT_BLUE = "\u001B[94m";
+  public static final String LIGHT_GREEN = "\u001B[92m";
+  public static final String LIGHT_CYAN = "\u001B[96m";
+  public static final String LIGHT_RED = "\u001B[91m";
+  public static final String LIGHT_PURPLE = "\u001B[95m";
+  public static final String LIGHT_YELLOW = "\u001B[93m";
+  public static final String BOLD = "\u001B[1m";
+  public static final String UNDERLINE = "\u001B[4m";
+
+  public static String minecraftToAnsiColor(String str) {
+    return str.replace("§0", BLACK)
+              .replace("§1", BLUE)
+              .replace("§2", GREEN)
+              .replace("§3", CYAN)
+              .replace("§4", RED)
+              .replace("§5", PURPLE)
+              .replace("§6", YELLOW)
+              .replace("§7", BLACK)
+              .replace("§8", BLACK)
+              .replace("§9", BLUE)
+              .replace("§a", LIGHT_GREEN)
+              .replace("§b", LIGHT_CYAN)
+              .replace("§c", LIGHT_RED)
+              .replace("§d", LIGHT_PURPLE)
+              .replace("§e", LIGHT_YELLOW)
+              .replace("§f", RESET);
+  }
 }
